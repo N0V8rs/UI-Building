4e60a9812f119da79eb01fc9b8de1899cdeec54d
N0V8rs <noahvatersnscc@gmail.com>
2023-09-18 15:14:27 -0300

    Testing

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float health;
+            health = 100;
+            float damage;
+            damage = 100;
+            int monsterdamage;
+            monsterdamage = 75;
+
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Damage: " + damage);
+            Console.WriteLine();
+            Console.WriteLine("Monster deals 75 damage");
+            Console.WriteLine();
+            health = health - monsterdamage;
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Damage: " + damage);
+
+
+            Console.ReadKey();
+        }
+    }
+}
